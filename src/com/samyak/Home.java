2af0d918fd79d762e5f782eb1100a4896b01b840
Samyak Jain <samyakjain3098@gmail.com>
2017-09-23 12:27:27 +0530

    created package

--- src/com/samyak/Home.java
+++ src/com/samyak/Home.java
@@ -1,9 +1,9 @@
+package com.samyak;
+
 import javax.swing.*;
 
 public class Home {
     private JPanel homePanel;
     private JTree tree1;
     private JTabbedPane tabbedPane1;
-    private JButton button1;
-    private JButton button2;
 }
